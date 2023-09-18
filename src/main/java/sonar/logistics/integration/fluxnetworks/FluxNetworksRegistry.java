d735bb290cbd3f15161d4827227480dc02cbbfa1
Zengyj <zengyj11111@gmail.com>
2023-09-18 17:32:03 +0800

    Fix ClassNotDef Exception,and fix build
    Sonar references to require the use of local deobf jar

--- src/main/java/sonar/logistics/integration/fluxnetworks/FluxNetworksRegistry.java
+++ src/main/java/sonar/logistics/integration/fluxnetworks/FluxNetworksRegistry.java
@@ -1,12 +1,12 @@
 package sonar.logistics.integration.fluxnetworks;
 
 import com.google.common.collect.Lists;
-import sonar.flux.api.AccessType;
-import sonar.flux.api.network.EnergyStats;
-import sonar.flux.api.network.IFluxNetwork;
-import sonar.flux.api.tiles.IFlux;
-import sonar.flux.api.tiles.IFlux.ConnectionType;
-import sonar.flux.connection.transfer.stats.NetworkStatistics;
+import sonar.fluxnetworks.api.network.AccessLevel;
+//import sonar.fluxnetworks.api.network.EnergyStats;
+import sonar.fluxnetworks.api.network.IFluxNetwork;
+import sonar.fluxnetworks.api.tiles.IFluxConnector;
+import sonar.fluxnetworks.api.network.ConnectionType;
+import sonar.fluxnetworks.common.connection.NetworkStatistics;
 import sonar.logistics.api.asm.ASMInfoRegistry;
 import sonar.logistics.api.core.tiles.displays.info.ClientNameConstants;
 import sonar.logistics.api.core.tiles.displays.info.register.IInfoRegistry;
@@ -19,22 +19,22 @@ public class FluxNetworksRegistry implements IInfoRegistry {
 	public void registerBaseReturns(IMasterInfoRegistry registry) {
 		registry.registerValidReturn(IFluxNetwork.class);
 		registry.registerValidReturn(ConnectionType.class);
-		registry.registerValidReturn(AccessType.class);
+		registry.registerValidReturn(AccessLevel.class);
 		registry.registerValidReturn(NetworkStatistics.class);
-		registry.registerValidReturn(EnergyStats.class);
+//		registry.registerValidReturn(EnergyStats.class);
 	}
 
 	public void registerBaseMethods(IMasterInfoRegistry registry) {
-		registry.registerMethods(IFlux.class, RegistryType.TILE, Lists.newArrayList("getCoords", "getNetwork", "getConnectionType", "getTransferLimit", "getCurrentTransferLimit", "getCurrentPriority", "getCustomName"), false);
-		registry.registerMethods(IFluxNetwork.class, RegistryType.TILE, Lists.newArrayList("getAccessType", "getNetworkID", "getNetworkName", "getCachedPlayerName", "getEnergyAvailable", "getMaxEnergyStored", "getStatistics"), false);
-		registry.registerMethods(AccessType.class, RegistryType.TILE, Lists.newArrayList("name"), false);
-		registry.registerMethods(NetworkStatistics.class, RegistryType.TILE, Lists.newArrayList("getLatestStats"), false);
-		registry.registerMethods(EnergyStats.class, RegistryType.TILE, Lists.newArrayList("getLatestStats"), false);
+		registry.registerMethods(IFluxConnector.class, RegistryType.TILE, Lists.newArrayList("getCoords", "getNetwork", "getConnectionType", "getMaxTransferLimit", "getRawLimit", "getRawPriority", "getCustomName"), false);
+		registry.registerMethods(IFluxNetwork.class, RegistryType.TILE, Lists.newArrayList("getMemberPermission", "getNetworkID", "getNetworkName"), false);
+		registry.registerMethods(AccessLevel.class, RegistryType.TILE, Lists.newArrayList("getName"), false);
+//		registry.registerMethods(NetworkStatistics.class, RegistryType.TILE, Lists.newArrayList("getLatestStats"), false);
+//		registry.registerMethods(EnergyStats.class, RegistryType.TILE, Lists.newArrayList("getLatestStats"), false);
 
 	}
 
 	public void registerAllFields(IMasterInfoRegistry registry) {
-		registry.registerFields(EnergyStats.class, RegistryType.TILE, Lists.newArrayList("transfer", "maxSent", "maxReceived"));
+//		registry.registerFields(EnergyStats.class, RegistryType.TILE, Lists.newArrayList("transfer", "maxSent", "maxReceived"));
 	}
 
 	public void registerAdjustments(IMasterInfoRegistry registry) {
